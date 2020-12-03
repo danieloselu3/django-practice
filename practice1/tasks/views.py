4dda0f51676fb3b2130e1ca1fc2aa1ee57cc4f3c
Daniel Oselu <doselu3@gmail.com>
2020-12-03 22:07:36 +0300

    add links

--- practice1/tasks/views.py
+++ practice1/tasks/views.py
@@ -8,3 +8,7 @@ def index(request):
     return render(request, 'tasks/index.html', {
         "tasks": tasks
     })
+
+# add task view
+def add(request):
+    return render(request, "tasks/add.html")
