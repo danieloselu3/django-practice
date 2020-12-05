6fbbdc718aa0d6de1c23e929c49b782c05abcaca
Daniel Oselu <doselu3@gmail.com>
2020-12-05 23:16:31 +0300

    adding form validation

--- practice1/tasks/views.py
+++ practice1/tasks/views.py
@@ -1,7 +1,12 @@
+from django import forms
 from django.shortcuts import render
 
 
 tasks = ['foo', 'bar', 'baz']
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -11,4 +16,15 @@ def index(request):
 
 # add task view
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid:
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form":form
+            })
+    return render(request, "tasks/add.html", {
+        "form":NewTaskForm()
+    })
